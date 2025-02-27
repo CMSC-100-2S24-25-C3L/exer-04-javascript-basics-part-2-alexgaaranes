0858079ff2d544b5ccaa357e858b4e930b0279d8
alexgaaranes <aaaranes@up.edu.ph>
2025-02-27 14:04:12 +0800

    feat: functions added

---
+++ index.js
@@ -0,0 +1,30 @@
+// This javascript file contains the functions to be called in test.js
+import { v4 as uuidv4 } from 'uuid';
+import validator from 'validator';
+import { appendFileSync } from 'node:fs';
+
+// Generate a unique ID from valid firstName and lastName
+const generateUniqueID = (firstName, lastName) => {
+    return firstName[0].toLowerCase() + lastName.toLowerCase() + uuidv4().slice(0,8);
+}
+
+// Adds an account that passed the validation to the user.txt
+const addAccount = ([firstName, lastName, email, age]) => {
+    if ((firstName === '') || (lastName === '') ||
+        (email === '') || (!validator.isEmail(email)) || 
+        (age <= 18)) return false;
+    
+    // Process the data string to write
+    let userData = `${firstName},${lastName},${email},${generateUniqueID(firstName,lastName)}`;
+    console.log(userData);
+
+    try{
+        appendFileSync("./users.txt", userData);
+    } catch (err){
+        console.log(err);
+    } finally{
+        return false;
+    }
+}
+
+export default { addAccount }
---
+++ test.js
@@ -0,0 +1,5 @@
+import index from './index.js'
+
+const credentials = ["Alex","Aranes","alexAranes@gmail.com",18];
+
+index.addAccount(credentials);
